602dbe08fee87a600adb215b2d29b940a4007ee0
Niels Vandekeybus <progster@gmail.com>
2018-09-24 09:13:48 +0200

    clean up tab handler

--- addon/utils/tab-handler.js
+++ addon/utils/tab-handler.js
@@ -1,6 +1,6 @@
 import EmberObject from '@ember/object';
 import HandlerResponse from './handler-response';
-import { tagName, isVoidElement, insertTextNodeWithSpace } from './dom-helpers';
+import nextTextNode from './next-text-node';
 
 export default EmberObject.extend({
   isHandlerFor(event) {
@@ -11,67 +11,11 @@ export default EmberObject.extend({
     const nextNode = this.nextNode(currentNode);
     this.rawEditor.updateRichNode();
     this.rawEditor.setCarret(nextNode, 0);
-    console.log(nextNode.parentNode);
+    console.log(nextNode);
     return new HandlerResponse({ allowPropagation: false});
   },
   nextNode(current) {
-    if (current.nodeType === Node.ELEMENT_NODE && current.firstChild) {
-      // current node has children, move down
-      if (current.firstChild.nodeType === Node.TEXT_NODE) {
-        return current.firstChild;
-      }
-      else if (current.firstChild.nodeType === Node.ELEMENT_NODE && tagName(current.firstChild) === 'li') {
-        return this.nextNode(current.firstChild);
-      }
-      else {
-        return insertTextNodeWithSpace(current,current.firstChild);
-      }
-    }
-    else if (current.nodeType === Node.ELEMENT_NODE) {
-      // current node is an element without children
-      if (isVoidElement(current)) {
-        // current node is a void node, create text node after
-        if (current.nextSibling.nodeType === Node.TEXT_NODE)
-          return current.nextSibling;
-        else
-          return insertTextNodeWithSpace(current.parentNode, current, true);
-      }
-      else {
-        // create text node in empty tag
-        const newNode = document.createTextNode();
-        current.appendChild(newNode);
-        return newNode;
-      }
-    }
-    else if (current.nodeType === Node.TEXT_NODE) {
-      // it's a text node
-      if (current.nextSibling) {
-        return this.nextNode(current.nextSibling);
-      }
-      else if (current.parentNode === this.rawEditor.rootNode) {
-        return current;
-      }
-      else {
-        // no sibling, parent is not the editor
-        var parent = current.parentNode;
-        if (parent.nextSibling && parent.nextSibling.nodeType === Node.TEXT_NODE) {
-          return parent.nextSibling;
-        }
-        else if (parent.nextSibling && parent.nextSibling.nodeType === Node.ELEMENT_NODE && tagName(parent.nextSibling) === 'li') {
-          return this.nextNode(parent.nextSibling);
-        }
-        else {
-          return insertTextNodeWithSpace(parent.parentNode, parent, true);
-        }
-      }
-    }
-    else {
-      // it's an unsupported tag, skip
-      if (current.nextSibling)
-        return this.nextNode(current.nextSibling);
-      else
-        return this.nextNode(current.parentNode);
-    }
+    return nextTextNode(current, this.rawEditor.rootNode);
   }
 });
 

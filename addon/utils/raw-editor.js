2cf934ee3fb5cdcec1e4d096c5d5bb1f6345cefa
Niels Vandekeybus <progster@gmail.com>
2019-05-09 10:20:28 +0200

    use replaceWith polyfill from npm package
    currently still using an import statement, which may later move to a build
    config. See https://msn0.github.io/mdn-polyfills/ for more information about the package.

--- addon/utils/raw-editor.js
+++ addon/utils/raw-editor.js
@@ -32,37 +32,7 @@ import nextTextNode from './next-text-node';
 const HIGHLIGHT_DATA_ATTRIBUTE = 'data-editor-highlight';
 const NON_BREAKING_SPACE = '\u00A0';
 import { unorderedListAction, orderedListAction, indentAction, unindentAction } from './list-helpers';
-
-/* IE11 and safari polyfill from MDN */
-function ReplaceWithPolyfill() {
-  'use-strict'; // For safari, and IE > 10
-  var parent = this.parentNode, i = arguments.length, currentNode;
-  if (!parent) return;
-  if (!i) // if there are no arguments
-    parent.removeChild(this);
-  while (i--) { // i-- decrements i and returns the value of i before the decrement
-    currentNode = arguments[i];
-    if (typeof currentNode !== 'object'){
-      currentNode = this.ownerDocument.createTextNode(currentNode);
-    } else if (currentNode.parentNode){
-      currentNode.parentNode.removeChild(currentNode);
-    }
-    // the value of "i" below is after the decrement
-    if (!i) // if currentNode is the first argument (currentNode === arguments[0])
-      parent.replaceChild(currentNode, this);
-    else // if currentNode isn't the first
-      parent.insertBefore(this.previousSibling, currentNode);
-  }
-}
-if (!Element.prototype.replaceWith)
-    Element.prototype.replaceWith = ReplaceWithPolyfill;
-if (!CharacterData.prototype.replaceWith)
-    CharacterData.prototype.replaceWith = ReplaceWithPolyfill;
-if (!DocumentType.prototype.replaceWith)
-  DocumentType.prototype.replaceWith = ReplaceWithPolyfill;
-/* IE11 and safari polyfill */
-
-
+import 'mdn-polyfills/Node.prototype.replaceWith';
 
 function findNodesToHighlight( node, start, end ){
   // We need to highlight all portions of text based on the output

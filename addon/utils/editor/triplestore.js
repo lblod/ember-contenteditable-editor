e3ad4179e71dc03a0ca0501d8ac03d2e9e42b524
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-09-16 14:03:03 +0200

    refinement

--- addon/utils/editor/triplestore.js
+++ addon/utils/editor/triplestore.js
@@ -1,5 +1,10 @@
 import { analyse as scanContexts } from '@lblod/marawa/rdfa-context-scanner';
-
+/**
+ * Given a resource, find all triples defined in it.
+ * Note: this includes also the triples of the container wrapping these triples.
+ * @param { String } 'http://a/expanded/resource/uri'
+ * @return [ Object ] [{subject, predicate, object, datatype}]
+ */
 function triplesDefinedInResource( resourceUri ){
   let domNodes = scanContexts(this.rootNode).filter( c => c.context.slice(-1)[0].subject === resourceUri ).map( c => c.semanticNode.domNode );
 
@@ -8,7 +13,7 @@ function triplesDefinedInResource( resourceUri ){
   }, []);
 
   let triples = contexts.reduce((acc, d) => {
-    return [...acc, ...d.context];
+    return [...acc, ...d.context.slice(d.context.findIndex(isSubject(resourceUri)))]; //remove wrapping context.
   }, []);
 
   //Get unique values
@@ -25,6 +30,10 @@ function sortedTripleObject(triple){
     acc[k] = triple[k];
     return acc;
   } , {});
+}
+
+function isSubject(resourceUri){
+  return ( triple ) => triple.subject === resourceUri;
 }
 
 export { triplesDefinedInResource }
